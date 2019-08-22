9750e9d2130b576aa8f9ded0301091053e72c3c2
vernesong <42875168+vernesong@users.noreply.github.com>
2019-08-22 19:31:37 +0800

    firewall bugs fix

--- luci-app-openclash/files/usr/share/openclash/openclash_watchdog.sh
+++ luci-app-openclash/files/usr/share/openclash/openclash_watchdog.sh
@@ -25,7 +25,30 @@ fi
    zone_line=`iptables -t nat -nL PREROUTING --line-number |grep "zone" 2>/dev/null |awk '{print $1}' 2>/dev/null |awk 'END {print}'`
    op_line=`iptables -t nat -nL PREROUTING --line-number |grep "openclash" 2>/dev/null |awk '{print $1}' 2>/dev/null |awk 'END {print}'`
    if [ "$zone_line" -gt "$op_line" ]; then
+#ipv4
+      iptables -t nat -F openclash >/dev/null 2>&1
+
+      nat_clashs=$(iptables -nvL PREROUTING -t nat | sed 1,2d | sed -n '/openclash/=' | sort -r)
+      for nat_clash in $nat_clashs; do
+         iptables -t nat -D PREROUTING "$nat_clash" >/dev/null 2>&1
+      done
+      iptables -t nat -X openclash >/dev/null 2>&1
+      out_line=`iptables -t nat -nL OUTPUT --line-number |grep "198.18.0.0/16" 2>/dev/null |awk '{print $1}' 2>/dev/null |awk 'END {print}'`
+      iptables -t nat -D OUTPUT "$out_line"
+    
+#ipv6
+      ip6tables -t nat -F openclash >/dev/null 2>&1
+
+      nat6_clashs=$(ip6tables -nvL PREROUTING -t nat 2>/dev/null | sed 1,2d | sed -n '/openclash/=' | sort -r)
+      for nat6_clash in $nat6_clashs; do
+         ip6tables -t nat -D PREROUTING "$nat6_clash" >/dev/null 2>&1
+      done
+      ip6tables -t nat -X openclash >/dev/null 2>&1
+      
+      new_pre_line=`expr "$zone_line" + 1 2>/dev/null`
+      sed -i "/^ \{0,\}iptables -t nat -A PREROUTING -p tcp -j openclash/c\iptables -t nat -I PREROUTING ${new_pre_line} -p tcp -j openclash" /var/etc/openclash.include
       /etc/init.d/firewall restart >/dev/null 2>&1
+      /etc/init.d/miniupnpd restart >/dev/null 2>&1
       echo "[$LOGTIME] Watchdog: Restart Firewall For Enable Redirect." > /tmp/openclash.log
    fi
    sleep 60
