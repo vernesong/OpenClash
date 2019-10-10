aad4dff71a01acc87b042f234d63b3d93cdc77dc
vernesong <42875168+vernesong@users.noreply.github.com>
2019-10-10 13:35:33 +0800

    Some improvements

--- luci-app-openclash/files/usr/share/openclash/yml_change.sh
+++ luci-app-openclash/files/usr/share/openclash/yml_change.sh
@@ -68,11 +68,11 @@
        fi
     fi
     
-    if [ -z "$(grep '^log-level:' "$7")" ]; then
+    if [ -z "$(grep '^log-level: $12' "$7")" ]; then
        if [ ! -z "$(grep "^ \{0,\}log-level:" "$7")" ]; then
-          sed -i "s/^ \{0,\}log-level:/log-level:/" "$7"
+          sed -i "/^ \{0,\}log-level:/c\log-level: ${12}" "$7"
        else
-          sed -i "/^dns:/i\log-level: silent" "$7"
+          sed -i "/^dns:/i\log-level: ${12}" "$7"
        fi
     fi
     
