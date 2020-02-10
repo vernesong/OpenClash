2ad04beb6fab8655cb3a73ee489eb43ffb565e28
vernesong <42875168+vernesong@users.noreply.github.com>
2020-02-10 23:36:33 +0800

    typo fix

--- luci-app-openclash/files/usr/share/openclash/openclash_core.sh
+++ luci-app-openclash/files/usr/share/openclash/openclash_core.sh
@@ -12,7 +12,7 @@ if [ "$(/etc/openclash/clash -v 2>/dev/null |awk -F ' ' '{print $2}')" != "$(sed
    if [ "$CPU_MODEL" != 0 ]; then
    echo "开始下载 OpenClash 内核..." >$START_LOG
    curl -sL -m 10 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-"$CPU_MODEL".tar.gz -o /tmp/clash.tar.gz >/dev/null 2>&1
-   if [ "$?" -eq "0" ] && [ "$(ls -l /tmp/clash.tar.gz |awk '{print int($5/1024)}')" -ne 0 ]; then
+   if [ "$?" -eq "0" ] && [ -s "/tmp/clash.tar.gz" ]; then
       tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1\
       && rm -rf /tmp/clash.tar.gz >/dev/null 2>&1\
       && chmod 4755 /tmp/clash\
