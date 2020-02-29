ed1af13c755efff16322952a650a160f213e7b66
Chao Shen <shen218@gmail.com>
2020-02-29 16:35:01 +0800

    To get current lan ip address
    To get current lan ip address when interface is in dhcp mode.

--- luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
+++ luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
@@ -88,7 +88,7 @@ local function ConnersHua_return()
 end
 
 local function daip()
-	return luci.sys.exec("uci get network.lan.ipaddr 2>/dev/null |awk -F '/' '{print $1}' 2>/dev/null |tr -d '\n'")
+	return luci.sys.exec("ifstatus lan 2>/dev/null |  jsonfilter -e '@[\"ipv4-address\"][0].address' 2>/dev/null")
 end
 
 local function dase()
