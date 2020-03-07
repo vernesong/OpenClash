c71a8c2dd25e952b345f99fda696191167d37c98
vernesong <42875168+vernesong@users.noreply.github.com>
2020-03-07 21:01:33 +0800

    download file use http proxy while clash enable

--- luci-app-openclash/files/usr/share/openclash/openclash_rule.sh
+++ luci-app-openclash/files/usr/share/openclash/openclash_rule.sh
@@ -3,47 +3,61 @@
    LOGTIME=$(date "+%Y-%m-%d %H:%M:%S")
    LOG_FILE="/tmp/openclash.log"
    echo "开始获取使用中的第三方规则名称..." >$START_LOG
-   rule_source=$(uci get openclash.config.rule_source 2>/dev/null)
+   RUlE_SOURCE=$(uci get openclash.config.rule_source 2>/dev/null)
+   HTTP_PORT=$(uci get openclash.config.http_port 2>/dev/null)
+   PROXY_ADDR="127.0.0.1"
    echo "开始下载使用中的第三方规则..." >$START_LOG
-      if [ "$rule_source" = "lhie1" ]; then
-         curl -sL --connect-timeout 10 --retry 2 https://raw.githubusercontent.com/lhie1/Rules/master/Clash/Rule.yml -o /tmp/rules.yaml >/dev/null 2>&1
+      if [ "$RUlE_SOURCE" = "lhie1" ]; then
+      	 if pidof clash >/dev/null; then
+      	 	  curl -sL --connect-timeout 10 --retry 2 -x http://$PROXY_ADDR:$HTTP_PORT https://raw.githubusercontent.com/lhie1/Rules/master/Clash/Rule.yml -o /tmp/rules.yaml >/dev/null 2>&1
+      	 else
+            curl -sL --connect-timeout 10 --retry 2 https://raw.githubusercontent.com/lhie1/Rules/master/Clash/Rule.yml -o /tmp/rules.yaml >/dev/null 2>&1
+         fi
          sed -i '1i Rule:' /tmp/rules.yaml
-      elif [ "$rule_source" = "ConnersHua" ]; then
-         curl -sL --connect-timeout 10 --retry 2 https://raw.githubusercontent.com/ConnersHua/Profiles/master/Clash/Pro.yaml -o /tmp/rules.yaml >/dev/null 2>&1
+      elif [ "$RUlE_SOURCE" = "ConnersHua" ]; then
+      	 if pidof clash >/dev/null; then
+      	 	  curl -sL --connect-timeout 10 --retry 2 -x http://$PROXY_ADDR:$HTTP_PORT https://raw.githubusercontent.com/ConnersHua/Profiles/master/Clash/Pro.yaml -o /tmp/rules.yaml >/dev/null 2>&1
+      	 else
+            curl -sL --connect-timeout 10 --retry 2 https://raw.githubusercontent.com/ConnersHua/Profiles/master/Clash/Pro.yaml -o /tmp/rules.yaml >/dev/null 2>&1
+         fi
          sed -i -n '/^Rule:/,$p' /tmp/rules.yaml
-      elif [ "$rule_source" = "ConnersHua_return" ]; then
-         curl -sL --connect-timeout 10 --retry 2 https://raw.githubusercontent.com/ConnersHua/Profiles/master/Clash/BacktoCN.yaml -o /tmp/rules.yaml >/dev/null 2>&1
+      elif [ "$RUlE_SOURCE" = "ConnersHua_return" ]; then
+      	 if pidof clash >/dev/null; then
+      	 	  curl -sL --connect-timeout 10 --retry 2 -x http://$PROXY_ADDR:$HTTP_PORT https://raw.githubusercontent.com/ConnersHua/Profiles/master/Clash/BacktoCN.yaml -o /tmp/rules.yaml >/dev/null 2>&1
+      	 else
+            curl -sL --connect-timeout 10 --retry 2 https://raw.githubusercontent.com/ConnersHua/Profiles/master/Clash/BacktoCN.yaml -o /tmp/rules.yaml >/dev/null 2>&1
+         fi
          sed -i -n '/^Rule:/,$p' /tmp/rules.yaml
       fi
-   if [ "$?" -eq "0" ] && [ "$rule_source" != 0 ] && [ -s "/tmp/rules.yaml" ]; then
+   if [ "$?" -eq "0" ] && [ "$RUlE_SOURCE" != 0 ] && [ -s "/tmp/rules.yaml" ]; then
       echo "下载成功，开始预处理规则文件..." >$START_LOG
-      sed -i "/^Rule:/a\##source:${rule_source}" /tmp/rules.yaml >/dev/null 2>&1
+      sed -i "/^Rule:/a\##source:${RUlE_SOURCE}" /tmp/rules.yaml >/dev/null 2>&1
       echo "检查下载的规则文件是否有更新..." >$START_LOG
-      cmp -s /etc/openclash/"$rule_source".yaml /tmp/rules.yaml
+      cmp -s /etc/openclash/"$RUlE_SOURCE".yaml /tmp/rules.yaml
       if [ "$?" -ne "0" ]; then
          echo "检测到下载的规则文件有更新，开始替换..." >$START_LOG
-         mv /tmp/rules.yaml /etc/openclash/"$rule_source".yaml >/dev/null 2>&1
-         sed -i '/^Rule:/a\##updated' /etc/openclash/"$rule_source".yaml >/dev/null 2>&1
+         mv /tmp/rules.yaml /etc/openclash/"$RUlE_SOURCE".yaml >/dev/null 2>&1
+         sed -i '/^Rule:/a\##updated' /etc/openclash/"$RUlE_SOURCE".yaml >/dev/null 2>&1
          echo "替换成功，重新加载 OpenClash 应用新规则..." >$START_LOG
          /etc/init.d/openclash reload 2>/dev/null
-         echo "${LOGTIME} Other Rules Update Successful" >>$LOG_FILE
+         echo "${LOGTIME} Other Rules 【$RUlE_SOURCE】 Update Successful" >>$LOG_FILE
       else
          echo "检测到下载的规则文件没有更新，停止继续操作..." >$START_LOG
          rm -rf /tmp/rules.yaml >/dev/null 2>&1
-         echo "${LOGTIME} Updated Other Rules No Change, Do Nothing" >>$LOG_FILE
+         echo "${LOGTIME} Updated Other Rules 【$RUlE_SOURCE】 No Change, Do Nothing" >>$LOG_FILE
          sleep 10
          echo "" >$START_LOG
       fi
-   elif [ "$rule_source" = 0 ]; then
+   elif [ "$RUlE_SOURCE" = 0 ]; then
       echo "未启用第三方规则，更新程序终止！" >$START_LOG
       rm -rf /tmp/rules.yaml >/dev/null 2>&1
-      echo "${LOGTIME} Other Rules Not Enable, Update Stop" >>$LOG_FILE
+      echo "${LOGTIME} Other Rules 【$RUlE_SOURCE】 Not Enable, Update Stop" >>$LOG_FILE
       sleep 10
       echo "" >$START_LOG
    else
       echo "第三方规则下载失败，请检查网络或稍后再试！" >$START_LOG
       rm -rf /tmp/rules.yaml >/dev/null 2>&1
-      echo "${LOGTIME} Other Rules Update Error" >>$LOG_FILE
+      echo "${LOGTIME} Other Rules 【$RUlE_SOURCE】 Update Error" >>$LOG_FILE
       sleep 10
       echo "" >$START_LOG
    fi
