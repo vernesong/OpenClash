9fb0763421b8864a427fd9e4dc5f41173c07c5f4
vernesong <42875168+vernesong@users.noreply.github.com>
2020-03-07 21:01:33 +0800

    download file use http proxy while clash enable

--- luci-app-openclash/files/usr/share/openclash/clash_version.sh
+++ luci-app-openclash/files/usr/share/openclash/clash_version.sh
@@ -4,9 +4,15 @@ LAST_OPVER="/tmp/clash_last_version"
 LAST_VER=$(sed -n 1p "$LAST_OPVER" 2>/dev/null |awk -F '-' '{print $1$2}' 2>/dev/null |awk -F '.' '{print $2$3}' 2>/dev/null)
 CLASH_VERF=$(/etc/openclash/clash -v 2>/dev/null)
 CLASH_VER=$(echo "$CLASH_VERF" 2>/dev/null |awk -F ' ' '{print $2}' 2>/dev/null |awk -F '-' '{print $1$2}' 2>/dev/null |awk -F '.' '{print $2$3}' 2>/dev/null)
-version_url="https://raw.githubusercontent.com/vernesong/OpenClash/master/core_version"
+HTTP_PORT=$(uci get openclash.config.http_port 2>/dev/null)
+PROXY_ADDR="127.0.0.1"
+VERSION_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core_version"
 if [ "$CKTIME" != "$(grep "CheckTime" $LAST_OPVER 2>/dev/null |awk -F ':' '{print $2}')" ]; then
-   curl -sL --connect-timeout 10 --retry 2 "$version_url" -o $LAST_OPVER >/dev/null 2>&1
+	 if pidof clash >/dev/null; then
+      curl -sL --connect-timeout 10 --retry 2 -x http://$PROXY_ADDR:$HTTP_PORT "$VERSION_URL" -o $LAST_OPVER >/dev/null 2>&1
+   else
+      curl -sL --connect-timeout 10 --retry 2 "$VERSION_URL" -o $LAST_OPVER >/dev/null 2>&1
+   fi
    if [ "$?" -eq "0" ] && [ -s "$LAST_OPVER" ]; then
       echo "CheckTime:$CKTIME" >>$LAST_OPVER
    else
