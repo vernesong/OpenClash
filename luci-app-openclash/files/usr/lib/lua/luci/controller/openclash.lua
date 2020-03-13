b9a1e75a5a06e803f4dee4bff0edb6405f3c7495
vernesong <42875168+vernesong@users.noreply.github.com>
2020-03-13 11:15:53 +0800

    v0.36.10-beta

--- luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
+++ luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
@@ -91,15 +91,12 @@ end
 
 local function coremodel()
   local coremodel = luci.sys.exec("cat /proc/cpuinfo |grep 'cpu model' 2>/dev/null |awk -F ': ' '{print $2}' 2>/dev/null")
+  local coremodel2 = luci.sys.exec("opkg status libc 2>/dev/null |grep 'Architecture' |awk -F ': ' '{print $2}' 2>/dev/null")
   if not coremodel or coremodel == "" then
-     return luci.sys.exec("opkg status libc 2>/dev/null |grep 'Architecture' |awk -F ': ' '{print $2}' 2>/dev/null")
+     return coremodel2 .. "," .. coremodel2
   else
-     return coremodel
+     return coremodel .. "," .. coremodel2
   end
-end
-
-local function coremodel2()
-	return luci.sys.exec("opkg status libc 2>/dev/null |grep 'Architecture' |awk -F ': ' '{print $2}' 2>/dev/null")
 end
 
 local function corecv()
@@ -113,7 +110,7 @@ end
 local function corelv()
 	local new = luci.sys.call(string.format("sh /usr/share/openclash/clash_version.sh"))
 	local core_lv = luci.sys.exec("sed -n 1p /tmp/clash_last_version 2>/dev/null")
-	return core_lv..","..new
+	return core_lv .. "," .. new
 end
 
 local function opcv()
@@ -121,7 +118,9 @@ local function opcv()
 end
 
 local function oplv()
-   return luci.sys.exec("sh /usr/share/openclash/openclash_version.sh && sed -n 1p /tmp/openclash_last_version 2>/dev/null |sed 's/^v//g' 2>/dev/null")
+	 local new = luci.sys.call(string.format("sh /usr/share/openclash/openclash_version.sh"))
+	 local oplv = luci.sys.exec("sed -n 1p /tmp/openclash_last_version 2>/dev/null")
+   return oplv .. "," .. new
 end
 
 local function opup()
@@ -205,7 +204,6 @@ function action_update()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
 			coremodel = coremodel(),
-			coremodel2 = coremodel2(),
 			corecv = corecv(),
 			opcv = opcv(),
 			corever = corever(),
--- luci-app-openclash/files/usr/share/openclash/openclash_version.sh
+++ luci-app-openclash/files/usr/share/openclash/openclash_version.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 CKTIME=$(date "+%Y-%m-%d-%H")
 LAST_OPVER="/tmp/openclash_last_version"
+OP_CV=$(sed -n 1p /etc/openclash/openclash_version 2>/dev/null |awk -F '-' '{print $1}' |awk -F '.' '{print $2$3}')
+OP_LV=$(sed -n 1p $LAST_OPVER 2>/dev/null |awk -F '-' '{print $1}' |awk -F '.' '{print $2$3}')
 HTTP_PORT=$(uci get openclash.config.http_port 2>/dev/null)
 PROXY_ADDR="127.0.0.1"
 if [ -s "/tmp/openclash.auth" ]; then
@@ -15,16 +17,19 @@ if [ "$CKTIME" != "$(grep "CheckTime" $LAST_OPVER 2>/dev/null |awk -F ':' '{prin
       curl -sL --connect-timeout 10 --retry 2 "$VERSION_URL" -o $LAST_OPVER >/dev/null 2>&1
    fi
    if [ "$?" -eq "0" ] && [ -s "$LAST_OPVER" ]; then
-      if [ "$(sed -n 1p /etc/openclash/openclash_version 2>/dev/null |awk -F '-' '{print $1}' |awk -F '.' '{print $2$3}')" -ge "$(sed -n 1p $LAST_OPVER 2>/dev/null |awk -F '-' '{print $1}' |awk -F '.' '{print $2$3}')" ]; then
+      if [ "$OP_CV" -ge "$OP_LV" ]; then
          sed -i "/^https:/i\CheckTime:${CKTIME}" "$LAST_OPVER" 2>/dev/null
          sed -i '/^https:/,$d' $LAST_OPVER
       else
          sed -i "/^https:/i\CheckTime:${CKTIME}" "$LAST_OPVER" 2>/dev/null
+         return 2
       fi
    else
       rm -rf "$LAST_OPVER"
    fi
-elif [ "$(sed -n 1p /etc/openclash/openclash_version 2>/dev/null |awk -F '-' '{print $1}' |awk -F '.' '{print $2$3}')" -ge "$(sed -n 1p $LAST_OPVER 2>/dev/null |awk -F '-' '{print $1}' |awk -F '.' '{print $2$3}')" ]; then
+elif [ "$OP_CV" -ge "$OP_LV" ]; then
    sed -i '/^CheckTime:/,$d' $LAST_OPVER
    echo "CheckTime:$CKTIME" >>$LAST_OPVER
+elif [ "$OP_CV" -lt "$OP_LV" ]; then
+   return 2
 fi
