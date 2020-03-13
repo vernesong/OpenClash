adea70e8f6a17af33cf3b9b08772215cf8c9ac4d
thank243 <thank243@gmail.com>
2020-03-13 10:44:21 +0800

    Update openclash_version.sh
    修正开启仅允许内网后，代理失效问题。

--- luci-app-openclash/files/usr/share/openclash/openclash_version.sh
+++ luci-app-openclash/files/usr/share/openclash/openclash_version.sh
@@ -2,7 +2,7 @@
 CKTIME=$(date "+%Y-%m-%d-%H")
 LAST_OPVER="/tmp/openclash_last_version"
 HTTP_PORT=$(uci get openclash.config.http_port 2>/dev/null)
-PROXY_ADDR="127.0.0.1"
+lan_ip=$(uci get network.lan.ipaddr 2>/dev/null |awk -F '/' '{print $1}' 2>/dev/null)
 if [ -s "/tmp/openclash.auth" ]; then
    PROXY_AUTH=$(cat /tmp/openclash.auth |awk -F '- ' '{print $2}' |sed -n '1p' 2>/dev/null)
 fi
@@ -10,7 +10,7 @@ fi
 VERSION_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/version"
 if [ "$CKTIME" != "$(grep "CheckTime" $LAST_OPVER 2>/dev/null |awk -F ':' '{print $2}')" ]; then
    if pidof clash >/dev/null; then
-      curl -sL --connect-timeout 10 --retry 2 -x http://$PROXY_ADDR:$HTTP_PORT -U "$PROXY_AUTH" "$VERSION_URL" -o $LAST_OPVER >/dev/null 2>&1
+      curl -sL --connect-timeout 10 --retry 2 -x http://$lan_ip:$HTTP_PORT -U "$PROXY_AUTH" "$VERSION_URL" -o $LAST_OPVER >/dev/null 2>&1
    else
       curl -sL --connect-timeout 10 --retry 2 "$VERSION_URL" -o $LAST_OPVER >/dev/null 2>&1
    fi
