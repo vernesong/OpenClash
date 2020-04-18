5e26dec51f3b21eced1bf4a72a2f018b58c3080a
vernesong <42875168+vernesong@users.noreply.github.com>
2020-04-18 19:19:06 +0800

    add restore configration button

--- luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
+++ luci-app-openclash/files/usr/lib/lua/luci/controller/openclash.lua
@@ -21,6 +21,7 @@ function index()
 	entry({"admin", "services", "openclash", "coregameupdate"},call("action_core_game_update"))
 	entry({"admin", "services", "openclash", "ping"}, call("act_ping"))
 	entry({"admin", "services", "openclash", "download_game_rule"}, call("action_download_rule"))
+	entry({"admin", "services", "openclash", "restore"}, call("action_restore_config"))
 	entry({"admin", "services", "openclash", "settings"},cbi("openclash/settings"),_("Global Settings"), 30).leaf = true
 	entry({"admin", "services", "openclash", "servers"},cbi("openclash/servers"),_("Severs and Groups"), 40).leaf = true
 	entry({"admin", "services", "openclash", "game-settings"},cbi("openclash/game-settings"),_("Game Rules and Groups"), 50).leaf = true
@@ -194,6 +195,15 @@ function download_rule()
   return state
 end
 
+function action_restore_config()
+	luci.sys.call("/etc/init.d/openclash stop >/dev/null 2>&1")
+	luci.sys.call("cp '/usr/share/openclash/backup/openclash' '/etc/config/openclash' >/dev/null 2>&1 &")
+	luci.sys.call("cp '/usr/share/openclash/backup/openclash_custom_rules.list' '/etc/openclash/custom/openclash_custom_rules.list' >/dev/null 2>&1 &")
+	luci.sys.call("cp '/usr/share/openclash/backup/openclash_custom_rules_2.list' '/etc/openclash/custom/openclash_custom_rules_2.list' >/dev/null 2>&1 &")
+	luci.sys.call("cp '/usr/share/openclash/backup/openclash_custom_fake_black.conf' '/etc/openclash/custom/openclash_custom_fake_black.conf' >/dev/null 2>&1 &")
+	luci.sys.call("cp '/usr/share/openclash/backup/openclash_custom_hosts.list' '/etc/openclash/custom/openclash_custom_hosts.list' >/dev/null 2>&1 &")
+end
+
 function action_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
@@ -206,6 +216,7 @@ function action_status()
 		mode = mode();
 	})
 end
+
 function action_state()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json({
