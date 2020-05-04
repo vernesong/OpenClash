913bc9d26f088c922391b7ce2f85c7c13b715daf
vernesong <42875168+vernesong@users.noreply.github.com>
2020-05-05 02:35:49 +0800

    fix: disable real-ip with redir-tun mode

--- luci-app-openclash/files/usr/share/openclash/cfg_servers_address_fake_block.sh
+++ luci-app-openclash/files/usr/share/openclash/cfg_servers_address_fake_block.sh
@@ -4,7 +4,7 @@ status=$(ps|grep -c /usr/share/openclash/cfg_servers_address_fake_block.sh)
 [ "$status" -gt "3" ] && exit 0
 
 en_mode=$(uci get openclash.config.en_mode 2>/dev/null)
-if pidof clash >/dev/null && [ "$en_mode" != "redir-host" ]; then
+if pidof clash >/dev/null && [ -z "$(echo "$en_mode" |grep "redir-host")" ]; then
    rm -rf /tmp/dnsmasq.d/dnsmasq_openclash.conf >/dev/null 2>&1
    /usr/share/openclash/openclash_fake_block.sh
    mkdir -p /tmp/dnsmasq.d
