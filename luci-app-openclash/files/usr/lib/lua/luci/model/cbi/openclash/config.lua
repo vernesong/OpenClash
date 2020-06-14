3efd13b29a5581fa65e4266d9b14194fc7817b8f
vernesong <42875168+vernesong@users.noreply.github.com>
2020-06-15 06:06:35 +0800

    fix translation

--- luci-app-openclash/files/usr/lib/lua/luci/model/cbi/openclash/config.lua
+++ luci-app-openclash/files/usr/lib/lua/luci/model/cbi/openclash/config.lua
@@ -341,7 +341,7 @@ g[n].enable=false
 end
 end
 
-rule_form=SimpleForm("rule_provider_file_list",translate("Rule Provider File List"))
+rule_form=SimpleForm("rule_provider_file_list",translate("Rule Providers File List"))
 rule_form.reset=false
 rule_form.submit=false
 tb2=rule_form:section(Table,g)
