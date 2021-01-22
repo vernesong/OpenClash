f92b3e3d26a533f43cba920a018f3d7fd666ea2e
vernesong <42875168+vernesong@users.noreply.github.com>
2021-01-22 17:44:26 +0800

    change: add auto restart

--- luci-app-openclash/luasrc/model/cbi/openclash/other-rules-edit.lua
+++ luci-app-openclash/luasrc/model/cbi/openclash/other-rules-edit.lua
@@ -36,6 +36,10 @@ s = m:section(NamedSection, sid, "other_rules")
 s.anonymous = true
 s.addremove   = false
 
+o = s:option(Value, "Note", translate("Note"))
+o.default = "default"
+o.rmempty = false
+
 o = s:option(ListValue, "config", translate("Config File"))
 local e,a={}
 local groupnames,filename
