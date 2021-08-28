71aed1012f4d834735bbd6d2c2d9d0652395465f
vernesong <42875168+vernesong@users.noreply.github.com>
2021-08-28 12:36:43 +0800

    chore: update dashboard

--- luci-app-openclash/root/usr/share/openclash/yacd/assets/Select.ea969293.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./vendor.422da9ba.js";var t="_select_1mr76_1";function a({options:a,selected:o,onChange:r}){return e.exports.createElement("select",{className:t,value:o,onChange:r},a.map((([t,a])=>e.exports.createElement("option",{key:t,value:t},a))))}export{a as S};
