a400c23e40985a9c4911b5cbbcb794e0157683f9
vernesong <42875168+vernesong@users.noreply.github.com>
2021-12-10 00:29:25 +0800

    fix: client page use form section

--- luci-app-openclash/luasrc/controller/openclash.lua
+++ luci-app-openclash/luasrc/controller/openclash.lua
@@ -10,7 +10,7 @@ function index()
 	page = entry({"admin", "services", "openclash"}, alias("admin", "services", "openclash", "client"), _("OpenClash"), 50)
 	page.dependent = true
 	page.acl_depends = { "luci-app-openclash" }
-	entry({"admin", "services", "openclash", "client"},cbi("openclash/client"),_("Overviews"), 20).leaf = true
+	entry({"admin", "services", "openclash", "client"},form("openclash/client"),_("Overviews"), 20).leaf = true
 	entry({"admin", "services", "openclash", "status"},call("action_status")).leaf=true
 	entry({"admin", "services", "openclash", "state"},call("action_state")).leaf=true
 	entry({"admin", "services", "openclash", "startlog"},call("action_start")).leaf=true
