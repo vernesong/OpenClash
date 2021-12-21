ad1ad6eb66a12dff1e4ae4ecf48361a819c80a47
vernesong <42875168+vernesong@users.noreply.github.com>
2021-12-22 04:32:18 +0800

    chore: refine the append logic for custom rules #1856

--- luci-app-openclash/root/usr/share/openclash/yml_rules_change.sh
+++ luci-app-openclash/root/usr/share/openclash/yml_rules_change.sh
@@ -134,72 +134,67 @@ yml_other_set()
    rescue Exception => e
    puts '${LOGTIME} Error: Set Custom Rules Error,【' + e.message + '】'
    end
+   
+   begin
+      if Value.has_key?('rules') and not Value['rules'].to_a.empty? then
+         if Value['rules'].to_a.grep(/(?=.*198.18.0)(?=.*REJECT)/).empty? then
+            Value['rules']=Value['rules'].to_a.insert(0,'IP-CIDR,198.18.0.1/16,REJECT,no-resolve')
+         end
+      else
+         Value['rules']=%w(IP-CIDR,198.18.0.1/16,REJECT,no-resolve)
+      end;
+   rescue Exception => e
+      puts '${LOGTIME} Error: Set 198.18.0.1/16 REJECT Rule Error,【' + e.message + '】'
+   end
+      
    begin
    if $5 == 1 then
-      if Value.has_key?('rules') and not Value['rules'].to_a.empty? then
-         Value['rules']=Value['rules'].to_a.insert(0,
-         'DOMAIN-SUFFIX,awesome-hd.me,DIRECT',
-         'DOMAIN-SUFFIX,broadcasthe.net,DIRECT',
-         'DOMAIN-SUFFIX,chdbits.co,DIRECT',
-         'DOMAIN-SUFFIX,classix-unlimited.co.uk,DIRECT',
-         'DOMAIN-SUFFIX,empornium.me,DIRECT',
-         'DOMAIN-SUFFIX,gazellegames.net,DIRECT',
-         'DOMAIN-SUFFIX,hdchina.org,DIRECT',
-         'DOMAIN-SUFFIX,hdsky.me,DIRECT',
-         'DOMAIN-SUFFIX,icetorrent.org,DIRECT',
-         'DOMAIN-SUFFIX,jpopsuki.eu,DIRECT',
-         'DOMAIN-SUFFIX,icetorrent.org,DIRECT',
-         'DOMAIN-SUFFIX,keepfrds.com,DIRECT',
-         'DOMAIN-SUFFIX,madsrevolution.net,DIRECT',
-         'DOMAIN-SUFFIX,m-team.cc,DIRECT',
-         'DOMAIN-SUFFIX,nanyangpt.com,DIRECT',
-         'DOMAIN-SUFFIX,ncore.cc,DIRECT',
-         'DOMAIN-SUFFIX,open.cd,DIRECT',
-         'DOMAIN-SUFFIX,ourbits.club,DIRECT',
-         'DOMAIN-SUFFIX,passthepopcorn.me,DIRECT',
-         'DOMAIN-SUFFIX,privatehd.to,DIRECT',
-         'DOMAIN-SUFFIX,redacted.ch,DIRECT',
-         'DOMAIN-SUFFIX,springsunday.net,DIRECT',
-         'DOMAIN-SUFFIX,tjupt.org,DIRECT',
-         'DOMAIN-SUFFIX,totheglory.im,DIRECT',
-         'DOMAIN-KEYWORD,announce,DIRECT',
-         'DOMAIN-KEYWORD,torrent,DIRECT'
-         )
-         begin
-         match_group=Value['rules'].grep(/(MATCH|FINAL)/)[0]
-         if not match_group.empty? and not match_group.nil? then
-            common_port_group=match_group.split(',')[2] or common_port_group=match_group.split(',')[1]
-            if not common_port_group.empty? and not common_port_group.nil? then
-               ruby_add_index = Value['rules'].index(Value['rules'].grep(/(MATCH|FINAL)/).first)
-               ruby_add_index ||= -1
-               Value['rules']=Value['rules'].to_a.insert(ruby_add_index,
-               'DST-PORT,80,' + common_port_group,
-               'DST-PORT,443,' + common_port_group,
-               'DST-PORT,22,' + common_port_group
-               )
-            end
+      Value['rules']=Value['rules'].to_a.insert(1,
+      'DOMAIN-SUFFIX,awesome-hd.me,DIRECT',
+      'DOMAIN-SUFFIX,broadcasthe.net,DIRECT',
+      'DOMAIN-SUFFIX,chdbits.co,DIRECT',
+      'DOMAIN-SUFFIX,classix-unlimited.co.uk,DIRECT',
+      'DOMAIN-SUFFIX,empornium.me,DIRECT',
+      'DOMAIN-SUFFIX,gazellegames.net,DIRECT',
+      'DOMAIN-SUFFIX,hdchina.org,DIRECT',
+      'DOMAIN-SUFFIX,hdsky.me,DIRECT',
+      'DOMAIN-SUFFIX,icetorrent.org,DIRECT',
+      'DOMAIN-SUFFIX,jpopsuki.eu,DIRECT',
+      'DOMAIN-SUFFIX,icetorrent.org,DIRECT',
+      'DOMAIN-SUFFIX,keepfrds.com,DIRECT',
+      'DOMAIN-SUFFIX,madsrevolution.net,DIRECT',
+      'DOMAIN-SUFFIX,m-team.cc,DIRECT',
+      'DOMAIN-SUFFIX,nanyangpt.com,DIRECT',
+      'DOMAIN-SUFFIX,ncore.cc,DIRECT',
+      'DOMAIN-SUFFIX,open.cd,DIRECT',
+      'DOMAIN-SUFFIX,ourbits.club,DIRECT',
+      'DOMAIN-SUFFIX,passthepopcorn.me,DIRECT',
+      'DOMAIN-SUFFIX,privatehd.to,DIRECT',
+      'DOMAIN-SUFFIX,redacted.ch,DIRECT',
+      'DOMAIN-SUFFIX,springsunday.net,DIRECT',
+      'DOMAIN-SUFFIX,tjupt.org,DIRECT',
+      'DOMAIN-SUFFIX,totheglory.im,DIRECT',
+      'DOMAIN-KEYWORD,announce,DIRECT',
+      'DOMAIN-KEYWORD,torrent,DIRECT',
+      'DOMAIN-KEYWORD,tracker,DIRECT'
+      )
+      match_group=Value['rules'].grep(/(MATCH|FINAL)/)[0]
+      if not match_group.nil? then
+         common_port_group=match_group.split(',')[2] or common_port_group=match_group.split(',')[1]
+         if not common_port_group.nil? then
+            ruby_add_index = Value['rules'].index(Value['rules'].grep(/(MATCH|FINAL)/).first)
+            ruby_add_index ||= -1
+            Value['rules']=Value['rules'].to_a.insert(ruby_add_index,
+            'DST-PORT,80,' + common_port_group,
+            'DST-PORT,443,' + common_port_group,
+            'DST-PORT,22,' + common_port_group
+            )
          end
-         rescue Exception => e
-         puts '${LOGTIME} Error: Set BT/P2P DIRECT Rules Error,【' + e.message + '】'
-         end
-         Value['rules'].to_a.collect!{|x|x.to_s.gsub(/(^MATCH.*|^FINAL.*)/, 'MATCH,DIRECT')}
-      else
-         puts '${LOGTIME} Warning: Because of No Rules Field, Stop Setting BT/P2P DIRECT Rules!'
-      end;
-   end;
-   rescue Exception => e
-   puts '${LOGTIME} Error: Set BT/P2P DIRECT Rules Error,【' + e.message + '】'
-   end
-   begin
-   if Value.has_key?('rules') and not Value['rules'].to_a.empty? then
-      if Value['rules'].to_a.grep(/(?=.*198.18.0)(?=.*REJECT)/).empty? then
-         Value['rules']=Value['rules'].to_a.insert(0,'IP-CIDR,198.18.0.1/16,REJECT,no-resolve')
       end
-   else
-      Value['rules']=%w(IP-CIDR,198.18.0.1/16,REJECT,no-resolve)
+      Value['rules'].to_a.collect!{|x|x.to_s.gsub(/(^MATCH.*|^FINAL.*)/, 'MATCH,DIRECT')}
    end;
    rescue Exception => e
-   puts '${LOGTIME} Error: Set 198.18.0.1/16 REJECT Rule Error,【' + e.message + '】'
+      puts '${LOGTIME} Error: Set BT/P2P DIRECT Rules Error,【' + e.message + '】'
    ensure
    File.open('$4','w') {|f| YAML.dump(Value, f)}
    end" 2>/dev/null >> $LOG_FILE
@@ -244,7 +239,7 @@ yml_other_rules_get()
    config_get "PayPal" "$section" "PayPal" ""
    config_get "Domestic" "$section" "Domestic" ""
    config_get "Others" "$section" "Others" ""
-   config_get "FCM" "$section" "FCM" ""
+   config_get "FCM" "$section" "FCM" "DIRECT"
 }
 
 if [ "$2" != "0" ]; then
