92b8ee2d09d884b6320086b7a6379a3d4f323bc6
vernesong <42875168+vernesong@users.noreply.github.com>
2021-12-28 04:31:02 +0800

    fix #1878

--- luci-app-openclash/luasrc/model/cbi/openclash/servers-config.lua
+++ luci-app-openclash/luasrc/model/cbi/openclash/servers-config.lua
@@ -316,11 +316,7 @@ o.default = "false"
 o:value("true")
 o:value("false")
 o:depends("obfs", "websocket")
-o:depends("obfs_vmess", "none")
-o:depends("obfs_vmess", "websocket")
-o:depends("obfs_vmess", "http")
-o:depends("obfs_vmess", "grpc")
-o:depends("obfs_vmess", "h2")
+o:depends("type", "vmess")
 o:depends("type", "socks5")
 o:depends("type", "http")
 
@@ -328,8 +324,9 @@ o = s:option(Value, "servername", translate("servername"))
 o.rmempty = true
 o.datatype = "host"
 o.placeholder = translate("example.com")
-o:depends("obfs_vmess", "websocket")
-o:depends("obfs_vmess", "grpc")
+o:depends({obfs_vmess = "websocket", tls = "true"})
+o:depends({obfs_vmess = "grpc", tls = "true"})
+o:depends({obfs_vmess = "none", tls = "true"})
 
 o = s:option(Value, "keep_alive", translate("keep-alive"))
 o.rmempty = true
