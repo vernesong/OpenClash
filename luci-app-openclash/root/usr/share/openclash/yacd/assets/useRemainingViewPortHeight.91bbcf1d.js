75df2dee4410e6c44b116c1c95b3a257250cfca5
vernesong <42875168+vernesong@users.noreply.github.com>
2022-03-12 20:50:38 +0800

    chore: update yacd

---
+++ luci-app-openclash/root/usr/share/openclash/yacd/assets/useRemainingViewPortHeight.91bbcf1d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor.3cf270b6.js";const{useState:s,useRef:u,useCallback:a,useLayoutEffect:c}=r;function d(){const t=u(null),[n,i]=s(200),e=a(()=>{const{top:o}=t.current.getBoundingClientRect();i(window.innerHeight-o)},[]);return c(()=>(e(),window.addEventListener("resize",e),()=>{window.removeEventListener("resize",e)}),[e]),[t,n]}export{d as u};
