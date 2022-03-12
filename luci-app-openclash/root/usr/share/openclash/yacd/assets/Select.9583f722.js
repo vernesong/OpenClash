3123404560a0479eea6be766fa419f1986eb78be
vernesong <42875168+vernesong@users.noreply.github.com>
2022-03-12 20:50:38 +0800

    chore: update yacd

---
+++ luci-app-openclash/root/usr/share/openclash/yacd/assets/Select.9583f722.js
@@ -0,0 +1,1 @@
+import{r as t}from"./vendor.3cf270b6.js";const l="_select_1mr76_1";var o={select:l};function n({options:r,selected:s,onChange:c}){return t.exports.createElement("select",{className:o.select,value:s,onChange:c},r.map(([e,a])=>t.exports.createElement("option",{key:e,value:e},a)))}export{n as S};
