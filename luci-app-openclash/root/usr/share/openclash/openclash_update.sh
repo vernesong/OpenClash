eeafac80e6b0efad9a73a077dcc49284dae49ebf
vernesong <42875168+vernesong@users.noreply.github.com>
2022-04-03 01:30:42 +0800

    fix: ipk upgrade check error

--- luci-app-openclash/root/usr/share/openclash/openclash_update.sh
+++ luci-app-openclash/root/usr/share/openclash/openclash_update.sh
@@ -44,8 +44,8 @@ if [ "$(expr "$OP_LV" \> "$OP_CV")" -eq 1 ] && [ -f "$LAST_OPVER" ]; then
    fi
    if [ "$?" == "0" ] && [ -s "/tmp/openclash.ipk" ]; then
       LOG_OUT "【OpenClash - v$LAST_VER】Download Successful, Start Pre Update Test..."
-      opkg install /tmp/openclash.ipk --noaction >>$LOG_FILE
-      if [ "$?" != "0" ]; then
+      
+      if [ -z "$(opkg install /tmp/openclash.ipk --noaction 2>/dev/null |grep 'Upgrading luci-app-openclash on root' 2>/dev/null)" ]; then
          LOG_OUT "【OpenClash - v$LAST_VER】Pre Update Test Failed, The File is Saved in /tmp/opencrash.ipk, Please Try to Update Manually!"
          if [ "$(uci -q get openclash.config.config_reload)" -eq 0 ]; then
       	    /etc/init.d/openclash restart >/dev/null 2>&1 &
