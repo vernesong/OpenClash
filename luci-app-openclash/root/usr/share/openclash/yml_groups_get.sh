79263f12f177c313753afa8d83da79c05673d2ef
vernesong <42875168+vernesong@users.noreply.github.com>
2022-04-15 23:02:55 +0800

    chore: add group policy Filter for meta core

--- luci-app-openclash/root/usr/share/openclash/yml_groups_get.sh
+++ luci-app-openclash/root/usr/share/openclash/yml_groups_get.sh
@@ -218,6 +218,14 @@ do
    }.join;
    
    Thread.new{
+   #Policy Filter
+   if Value['proxy-groups'][$count].key?('filter') then
+      policy_filter = '${uci_set}policy_filter=' + Value['proxy-groups'][$count]['filter'].to_s
+      system(policy_filter)
+   end
+   }.join;
+   
+   Thread.new{
    #interface-name
    if Value['proxy-groups'][$count].key?('interface-name') then
       interface_name = '${uci_set}interface_name=' + Value['proxy-groups'][$count]['interface-name'].to_s
