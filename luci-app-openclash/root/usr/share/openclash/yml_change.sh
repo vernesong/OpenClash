a04dc42abadbf22b74ed14355fbe31df0dea5798
vernesong <42875168+vernesong@users.noreply.github.com>
2022-05-04 20:38:26 +0800

    chore: compatible for meta core changes

--- luci-app-openclash/root/usr/share/openclash/yml_change.sh
+++ luci-app-openclash/root/usr/share/openclash/yml_change.sh
@@ -337,8 +337,12 @@ Thread.new{
       if File::exist?('/etc/openclash/custom/openclash_sniffing_domain_filter.yaml') and ${24} == 1 then
          Value_7 = YAML.load_file('/etc/openclash/custom/openclash_sniffing_domain_filter.yaml');
          if Value_7 != false and not Value_7['skip-sni'].to_a.empty? then
-            Value['sniffer']['skip-sni']=Value_7['skip-sni'];
-            Value['sniffer']['skip-sni']=Value['sniffer']['skip-sni'].uniq;
+            Value['sniffer']['skip-domain']=Value_7['skip-sni'];
+            Value['sniffer']['skip-domain']=Value['sniffer']['skip-domain'].uniq;
+         end
+         if Value_7 != false and not Value_7['skip-domain'].to_a.empty? then
+            Value['sniffer']['skip-domain']=Value_7['skip-domain'];
+            Value['sniffer']['skip-domain']=Value['sniffer']['skip-domain'].uniq;
          end
       end;
       if File::exist?('/etc/openclash/custom/openclash_sniffing_port_filter.yaml') and ${24} == 1 then
