0d09172e454078a1709329e46c835700bfd9cd43
vernesong <42875168+vernesong@users.noreply.github.com>
2022-05-27 16:46:38 +0800

    chore: replace to fastly.jsdelivr.net default

--- luci-app-openclash/luasrc/model/cbi/openclash/settings.lua
+++ luci-app-openclash/luasrc/model/cbi/openclash/settings.lua
@@ -163,8 +163,8 @@ o.default = "0"
 o = s:taboption("settings", Value, "github_address_mod", font_red..bold_on..translate("Github Address Modify")..bold_off..font_off)
 o.description = translate("Modify The Github Address In The Config And OpenClash With Proxy(CDN) To Prevent File Download Faild. Format Reference:").." ".."<a href='javascript:void(0)' onclick='javascript:return winOpen(\"https://ghproxy.com/\")'>https://ghproxy.com/</a>"
 o:value("0", translate("Disable"))
-o:value("https://cdn.jsdelivr.net/")
 o:value("https://fastly.jsdelivr.net/")
+o:value("https://cdn.jsdelivr.net/")
 o.default = "0"
 
 o = s:taboption("settings", Value, "delay_start", translate("Delay Start (s)"))
