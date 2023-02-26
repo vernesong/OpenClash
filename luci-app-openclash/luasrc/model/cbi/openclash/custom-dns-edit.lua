a942128bc2a225cca50e47bac3a381c6cd93d897
vernesong <42875168+vernesong@users.noreply.github.com>
2023-02-26 09:37:02 +0800

    chore: refine

--- luci-app-openclash/luasrc/model/cbi/openclash/custom-dns-edit.lua
+++ luci-app-openclash/luasrc/model/cbi/openclash/custom-dns-edit.lua
@@ -13,7 +13,7 @@ bold_off = [[</strong>]]
 
 m = Map(openclash, translate("Add Custom DNS Servers"))
 m.pageaction = false
-m.redirect = luci.dispatcher.build_url("admin/services/openclash/settings")
+m.redirect = luci.dispatcher.build_url("admin/services/openclash/config-overwrite")
 if m.uci:get(openclash, sid) ~= "dns_servers" then
 	luci.http.redirect(m.redirect)
 	return
