abffd6f5e1718863e0ac069c711eadb703bacf53
snachx <snachx@gmail.com>
2024-03-14 01:36:46 +0800

    IPv6 support for dns debugging and skip proxy address feature (#3801)
    Skip Proxy Address feature relies on the openclash_debug_dns.lua script. This update  has added IPv6 support, enabling the Skip Proxy Address functionality to also support IPv6.

--- luci-app-openclash/root/usr/share/openclash/openclash_debug_dns.lua
+++ luci-app-openclash/root/usr/share/openclash/openclash_debug_dns.lua
@@ -22,61 +22,67 @@ local function debug_dns()
 	local passwd = uci:get("openclash", "config", "dashboard_password") or ""
 
 	if datatype.hostname(addr) and ip and port then
-		info = luci.sys.exec(string.format('curl -sL -m 3 -H "Content-Type: application/json" -H "Authorization: Bearer %s" -XGET http://"%s":"%s"/dns/query?name="%s"', passwd, ip, port, addr))
-		if info then
-			info = json.parse(info)
+		info_v4 = luci.sys.exec(string.format('curl -sL -m 3 -H "Content-Type: application/json" -H "Authorization: Bearer %s" -XGET http://"%s":"%s"/dns/query?name="%s"', passwd, ip, port, addr))
+		info_v6 = luci.sys.exec(string.format('curl -sL -m 3 -H "Content-Type: application/json" -H "Authorization: Bearer %s" -XGET \"http://"%s":"%s"/dns/query?name="%s"&type=AAAA\"', passwd, ip, port, addr))
+		if info_v4 then
+			info_v4 = json.parse(info_v4)
 		end
-		if info and not resolve then
-			print("Status: "..(info.Status))
-			print("TC: "..tostring(info.TC))
-			print("RD: "..tostring(info.RD))
-			print("RA: "..tostring(info.RA))
-			print("AD: "..tostring(info.AD))
-			print("CD: "..tostring(info.CD))
-			print("")
-			print("Question: ")
-			for _, v in pairs(info.Question) do
-				print("  Name: "..(v.Name))
-				print("  Qtype: "..(v.Qtype))
-				print("  Qclass: "..(v.Qclass))
+		if info_v6 then
+			info_v6 = json.parse(info_v6)
+		end
+		for _, info in pairs({info_v4, info_v6}) do
+			if info and not resolve then
+				print("Status: "..(info.Status))
+				print("TC: "..tostring(info.TC))
+				print("RD: "..tostring(info.RD))
+				print("RA: "..tostring(info.RA))
+				print("AD: "..tostring(info.AD))
+				print("CD: "..tostring(info.CD))
 				print("")
-			end
-			if info.Answer then
-				print("Answer: ")
-				for _, v in pairs(info.Answer) do
-					print("  TTL: "..(v.TTL))
-					print("  data: "..(v.data:gsub("\n?", "")))
-					print("  name: "..(v.name))
-					print("  type: "..(v.type))
+				print("Question: ")
+				for _, v in pairs(info.Question) do
+					print("  Name: "..(v.Name))
+					print("  Qtype: "..(v.Qtype))
+					print("  Qclass: "..(v.Qclass))
 					print("")
 				end
-			end
-			if info.Additional then
-				print("Additional: ")
-				for _, v in pairs(info.Additional) do
-					print("  TTL: "..(v.TTL))
-					print("  data: "..(v.data:gsub("\n?", "")))
-					print("  name: "..(v.name))
-					print("  type: "..(v.type))
-					print("")
+				if info.Answer then
+					print("Answer: ")
+					for _, v in pairs(info.Answer) do
+						print("  TTL: "..(v.TTL))
+						print("  data: "..(v.data:gsub("\n?", "")))
+						print("  name: "..(v.name))
+						print("  type: "..(v.type))
+						print("")
+					end
+				end
+				if info.Additional then
+					print("Additional: ")
+					for _, v in pairs(info.Additional) do
+						print("  TTL: "..(v.TTL))
+						print("  data: "..(v.data:gsub("\n?", "")))
+						print("  name: "..(v.name))
+						print("  type: "..(v.type))
+						print("")
+					end
+				end
+				if info.Authority then
+					print("Authority: ")
+					for _, v in pairs(info.Authority) do
+						print("  TTL: "..(v.TTL))
+						print("  data: "..(v.data:gsub("\n?", "")))
+						print("  name: "..(v.name))
+						print("  type: "..(v.type))
+						print("")
+					end
 				end
 			end
-			if info.Authority then
-				print("Authority: ")
-				for _, v in pairs(info.Authority) do
-					print("  TTL: "..(v.TTL))
-					print("  data: "..(v.data:gsub("\n?", "")))
-					print("  name: "..(v.name))
-					print("  type: "..(v.type))
-					print("")
-				end
-			end
-		end
-		if info and resolve then
-			if info.Answer then
-				for _, v in pairs(info.Answer) do
-					if v.type == 1 then
-						print(v.data)
+			if info and resolve then
+				if info.Answer then
+					for _, v in pairs(info.Answer) do
+						if v.type == 1 or v.type == 28 then
+							print(v.data)
+						end
 					end
 				end
 			end
