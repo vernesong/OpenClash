f8c9eb3d24488bf78eeea3fb58812bd204461516
vernesong <42875168+vernesong@users.noreply.github.com>
2024-10-02 16:47:28 +0800

    #4077

--- luci-app-openclash/root/usr/share/openclash/openclash_watchdog.sh
+++ luci-app-openclash/root/usr/share/openclash/openclash_watchdog.sh
@@ -66,6 +66,9 @@ if [ "$enable" -eq 1 ]; then
 	   CRASH_NUM=$(expr "$CRASH_NUM" + 1)
 	   if [ "$CRASH_NUM" -le 3 ]; then
          LOG_OUT "Watchdog: Clash Core Problem, Restart..."
+         ulimit -SHn 65535 2>/dev/null
+         ulimit -v unlimited 2>/dev/null
+         ulimit -u unlimited 2>/dev/null
          capabilties="cap_sys_resource,cap_dac_override,cap_net_raw,cap_net_bind_service,cap_net_admin,cap_sys_ptrace,cap_sys_admin"
          capsh --caps="${capabilties}+eip" -- -c "capsh --user=nobody --addamb='${capabilties}' -- -c 'nohup $CLASH -d $CLASH_CONFIG -f \"$CONFIG_FILE\" >> $LOG_FILE 2>&1 &'" >> $LOG_FILE 2>&1
 	      sleep 3
