5b9c3d6d1dda60a6251442832f92257143f10d02
vernesong <42875168+vernesong@users.noreply.github.com>
2025-05-02 03:15:39 +0800

    fix external-ui-name

--- luci-app-openclash/root/usr/share/openclash/yml_change.sh
+++ luci-app-openclash/root/usr/share/openclash/yml_change.sh
@@ -345,7 +345,7 @@ threads << Thread.new {
       Value['secret']='$2';
       Value['bind-address']='*';
       Value['external-ui']='/usr/share/openclash/ui';
-      Value['external-ui-name']='xd';
+      Value['external-ui-name']='metacubexd';
       if Value.key?('external-ui-url') then
          Value.delete('external-ui-url');
       end;
