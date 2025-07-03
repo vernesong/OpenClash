a6e99196afbff562f4681601cf17eceb76edcb03
vernesong <42875168+vernesong@users.noreply.github.com>
2025-07-04 04:22:38 +0800

    0.46.112

--- luci-app-openclash/root/usr/share/openclash/YAML.rb
+++ luci-app-openclash/root/usr/share/openclash/YAML.rb
@@ -3,6 +3,18 @@ module YAML
     alias_method :load, :unsafe_load if YAML.respond_to? :unsafe_load
   end
 
+  def dump(obj, io = nil, **options)
+      default_options = {
+        :line_width => -1,
+        :canonical => false
+      }
+      
+      merged_options = default_options.merge(options)
+      
+      original_dump(obj, io, **merged_options)
+    end
+  end
+
   def self.LOG(info)
     puts Time.new.strftime("%Y-%m-%d %H:%M:%S") + " #{info}"
   end
