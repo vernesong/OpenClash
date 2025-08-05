2edbafa31d5b2c35a4a98eb64e2797425d44fbb9
vernesong <42875168+vernesong@users.noreply.github.com>
2025-08-06 00:27:32 +0800

    chore: overwrite add smart settings tab

---
+++ luci-app-openclash/root/usr/share/openclash/openclash_lgbm.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+. /usr/share/openclash/openclash_ps.sh
+. /usr/share/openclash/log.sh
+. /usr/share/openclash/openclash_curl.sh
+
+set_lock() {
+   exec 868>"/tmp/lock/openclash_lgbm.lock" 2>/dev/null
+   flock -x 868 2>/dev/null
+}
+
+del_lock() {
+   flock -u 868 2>/dev/null
+   rm -rf "/tmp/lock/openclash_lgbm.lock" 2>/dev/null
+}
+
+set_lock
+inc_job_counter
+
+small_flash_memory=$(uci get openclash.config.small_flash_memory 2>/dev/null)
+LGBM_CUSTOM_URL=$(uci get openclash.config.lgbm_custom_url 2>/dev/null)
+restart=0
+
+if [ "$small_flash_memory" != "1" ]; then
+   lgbm_path="/etc/openclash/model.bin"
+   mkdir -p /etc/openclash
+else
+   lgbm_path="/tmp/etc/openclash/model.bin"
+   mkdir -p /tmp/etc/openclash
+fi
+LOG_OUT "Start Downloading LightGBM Model..."
+if [ -z "$LGBM_CUSTOM_URL" ]; then
+   DOWNLOAD_URL="https://github.com/vernesong/mihomo/releases/download/LightGBM-Model/Model.bin"
+else
+   DOWNLOAD_URL=$LGBM_CUSTOM_URL
+fi
+DOWNLOAD_FILE_CURL "$DOWNLOAD_URL" "/tmp/model.bin"
+if [ "$?" -eq 0 ] && [ -s "/tmp/model.bin" ]; then
+   LOG_OUT "LightGBM Model Download Success, Check Updated..."
+   cmp -s /tmp/model.bin "$lgbm_path"
+   if [ "$?" -ne "0" ]; then
+      LOG_OUT "LightGBM Model Has Been Updated, Starting To Replace The Old Version..."
+      rm -rf "/etc/openclash/model.bin"
+      mv /tmp/model.bin "$lgbm_path" >/dev/null 2>&1
+      LOG_OUT "LightGBM Model Update Successful!"
+      restart=1
+   else
+      LOG_OUT "Updated LightGBM Model No Change, Do Nothing..."
+   fi
+else
+   LOG_OUT "LightGBM Model Update Error, Please Try Again Later..."
+fi
+
+rm -rf /tmp/model.bin >/dev/null 2>&1
+
+SLOG_CLEAN
+dec_job_counter_and_restart "$restart"
+del_lock
