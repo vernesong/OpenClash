b82e0790c08b3a55cbf03dcd9b37e096d90b8d46
vernesong <42875168+vernesong@users.noreply.github.com>
2025-09-25 10:50:18 +0800

    chore: Zashboard update use gh-pages-cdn-fonts

--- luci-app-openclash/root/usr/share/openclash/openclash_download_dashboard.sh
+++ luci-app-openclash/root/usr/share/openclash/openclash_download_dashboard.sh
@@ -44,8 +44,8 @@
   elif [ "$DASH_NAME" == "Zashboard" ]; then
       UNPACK_FILE_DIR="/usr/share/openclash/ui/zashboard/"
       BACKUP_FILE_DIR="/usr/share/openclash/ui/zashboard_backup/"
-      DOWNLOAD_PATH="https://github.com/Zephyruso/zashboard/releases/latest/download/dist-cdn-fonts.zip"
-      FILE_PATH_INCLUDE="dist"
+      DOWNLOAD_PATH="https://codeload.github.com/Zephyruso/zashboard/zip/refs/heads/gh-pages-cdn-fonts"
+      FILE_PATH_INCLUDE="zashboard-gh-pages-cdn-fonts"
    else
       UNPACK_FILE_DIR="/usr/share/openclash/ui/metacubexd/"
       BACKUP_FILE_DIR="/usr/share/openclash/ui/metacubexd_backup/"
